a39a8ce9c0a5fc9beda73ffe524055de33c4895f
Mohammad Bilal Mirza <Mohammad Bilal Mirza@BILAL-PC>
2017-04-08 23:07:02 +0500

    Add project files.

---
+++ VideoConverter/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace VideoConverter
+{
+    partial class VideoConverterApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(88, 183);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(202, 36);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "wmv to mp4";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(298, 183);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(528, 36);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(314, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 89);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Run Proc";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // VideoConverterApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 462);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "VideoConverterApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Video Converter App";
+            this.Load += new System.EventHandler(this.VideoConverterApp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VideoConverter/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NReco.VideoConverter;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace VideoConverter
+{
+    public partial class VideoConverterApp : Form
+    {
+        public VideoConverterApp()
+        {
+            InitializeComponent();
+        }
+        // variable to store the filename and extension
+        string fName, fExt;
+        int p = -1;
+        private void VideoConverterApp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    // Wait 100 milliseconds.
+            //    Thread.Sleep(1000);
+            //    // Report progress.
+            //    if (p == -1)
+            //        backgroundWorker1.ReportProgress(i);
+            //    else
+            //    {
+            //        backgroundWorker1.ReportProgress(p);
+            //        break;
+            //    }
+            //}
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            //progressBar1.Value = e.ProgressPercentage;
+            //// Set the text.
+            //this.Text = e.ProgressPercentage.ToString();
+        }
+
+        /// <summary>
+        /// Run Process
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var result = openFileDialog1.ShowDialog();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fi = new FileInfo(ofd.FileName);
+                fExt = fi.Extension;
+                fName = fi.FullName.Substring(0, fi.FullName.Length - fExt.Length);
+                try
+                {
+
+                    
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.GetBaseException().Message, @"Error", MessageBoxButtons.OK);
+                }
+
+
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var result = openFileDialog1.ShowDialog();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fi = new FileInfo(ofd.FileName);
+                fExt = fi.Extension;
+                fName = fi.FullName.Substring(0, fi.FullName.Length - fExt.Length);
+                //if (!fExt.Equals(".wmv"))
+                //{
+                //    MessageBox.Show("Only WMV file type is supported");
+                //}
+                //else
+                //{ }
+                try
+                {
+
+                    var x = false;
+                    var message = "";
+                    var input = ofd.FileName;//Path.Combine(@"D:\Dev\Company\Exalted Solutions 2016 July\Projects\CaliberMatrix-Development\CaliberMatrix\SeleniumTestCaseRecording\SeleniumTest-3034ef9ce96dfb5b02fb76a04851ae61", "Video-2017-04-07_12-02-10-AM.wmv");
+                    if (!File.Exists(input))
+                    {
+                        MessageBox.Show(@"File does not exist", @"Error", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        var fileName = Path.GetFileNameWithoutExtension(input);
+
+                        var outputFileNameOgg = fileName + ".ogg";
+                        var outputFileNameMp4 = fileName + ".mp4";
+                        var outputFileNameWebm = fileName + ".webm";
+
+                        var bytes = File.ReadAllBytes(input);
+                        var data = new MemoryStream(bytes);
+                        var t = Task.Factory.StartNew(() =>
+                         {
+                             try
+                             {
+                                 //backgroundWorker1.RunWorkerAsync();
+                                 var ffMpeg = new FFMpegConverter();
+                                 //ffMpeg.ConvertProgress +=
+                                 ffMpeg.ConvertMedia(input, outputFileNameWebm, Format.webm);
+                                 ffMpeg.ConvertMedia(input, outputFileNameOgg, Format.ogg);
+                                 ffMpeg.ConvertMedia(input, outputFileNameMp4, Format.mp4);
+
+                                 x = true;
+                                 p = 100;
+                                 message = "Video converted successfully";
+                                 return x;
+                             }
+                             catch (Exception ex)
+                             {
+                                 p = 100;
+                                 x = false;
+                                 message = ex.GetBaseException().Message;
+                                 return x;
+                             }
+                         });
+                        t.Wait();
+                        if (t.IsCompleted)
+                            if (x)
+                            {
+                                MessageBox.Show(message, @"Success", MessageBoxButtons.OK);
+                            }
+                            else
+                            {
+                                MessageBox.Show(message, @"Error", MessageBoxButtons.OK);
+                            }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.GetBaseException().Message, @"Error", MessageBoxButtons.OK);
+                }
+
+
+            }
+        }
+    }
+}
